881021739678abbcf69d6844be16c16c93ea1684
Meryem Yıldırım <mrymmyldrm@gmail.com>
2023-02-16 18:28:19 +0300

    Update

--- src/main/java/com/i2i/ocs/OcsApplication.java
+++ src/main/java/com/i2i/ocs/OcsApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OcsApplication {
+	public class OcsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcsApplication.class, args);
 	}
+
+
 }
